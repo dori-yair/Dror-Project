d6bd9d525bd0ba3b865e6dbfa5bcb722d0cc7ae3
Dori <dori.yair@gmail.com>
2020-07-25 14:48:30 +0300

    added text input

--- components/FormScreen.js
+++ components/FormScreen.js
@@ -1,37 +1,66 @@
-import React, { Component } from 'react';
-import { StyleSheet, Text, View, Image, TouchableHighlight } from 'react-native';
- 
+import React, { Component,useState } from 'react';
+import { StyleSheet, Text, View, TextInput, Button,TouchableHighlight } from 'react-native';
+
 
 class FormScreen extends Component {
-  //state = {}
-    
   constructor(props) {
     super(props);
+    this.state = {
+      title: 'title',
+      eventStart:'',
+      eventEnd:'',
+    };
    }
 
-  componentWillUnmount() {
-    
-  }
-
+  componentWillUnmount() {}
+  OnSubmit(){}
   render() {
     return (
+
       <View style={styles.container}>
-        <Text>Welcome to add new event screen</Text>
+        
+        <TextInput
+          placeholder="שם האירוע"  
+          style={styles.inputText}
+          onChangeText={ (text)=> this.setState({title:text})}
+        />
+        
+        <Button
+          title={'הוסף'}
+          style={styles.input}
+          
+        />
       </View>
+
+
     );
   }
 }
 
+
 const styles = StyleSheet.create({
   
   container: {
-    flex: 0,
-    justifyContent: 'center',
-    alignItems: 'center',
+    flex: 1,
+        alignItems: 'flex-start',
     backgroundColor: '#F5FCff',
-
+    borderWidth: 2,
+    padding: 10,
   },
-
+  input:{
+    backgroundColor: 'skyblue',
+  },
+  inputText: {
+    padding: 10,
+    textAlign:'right',
+    borderWidth: 1,
+    borderColor: 'skyblue',
+    borderTopColor:'transparent',
+    borderRightColor:'transparent',
+    borderLeftColor:'transparent',
+    margin: 20,
+    width:270,
+  },
 });
 
 export default FormScreen;
