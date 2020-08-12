e0a65ca0375900d3149e6b1b8b64cffaf41364c3
Dori <dori.yair@gmail.com>
2020-08-12 19:38:23 +0300

    Added event list screen

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ import {StyleSheet} from 'react-native';
 
 import AddEventScreen from './components/AddEventScreen';
 import HomeScreen from './components/HomeScreen'
+import EventListScreen from './components/EventListScreen';
 
 const Stack = createStackNavigator();
 
@@ -14,6 +15,7 @@ export default function App () {
     <Stack.Navigator initialRouteName="מסך הבית">
       <Stack.Screen name="מסך הבית" component={HomeScreen} />
       <Stack.Screen name="הוסף אירוע" component={AddEventScreen} />
+      <Stack.Screen name="רשימת אירועים" component={EventListScreen} />
     </Stack.Navigator>
   </NavigationContainer>
 
--- components/HomeScreen.js
+++ components/HomeScreen.js
@@ -12,11 +12,15 @@ import {
 export function HomeScreen({ navigation }) {
     return (
       <View style={styles.container}>
-        <Text style={{ fontSize: '24' }} >שלום וברוך הבא</Text>
+        <Text style={{ fontSize: 24 }} >שלום וברוך הבא</Text>
         
         <Button
           title="הוסף אירוע"
           onPress={() => navigation.navigate('הוסף אירוע')}
+        />
+        <Button
+          title="רשימת אירועים"
+          onPress={() => navigation.navigate('רשימת אירועים')}
         />
       </View>
     );
